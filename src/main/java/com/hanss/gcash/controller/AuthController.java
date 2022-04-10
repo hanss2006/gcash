e3d4bf616205e218523e7ad26e51d38370939e73
hanss <hanss@list.ru>
2022-04-10 21:26:20 +0300

    Initial commit

--- src/main/java/com/hanss/gcash/controller/AuthController.java
+++ src/main/java/com/hanss/gcash/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.hanss.gcash.controller;
 
 import com.hanss.gcash.security.jwt.JwtUtils;
+import com.hanss.gcash.security.payload.request.LoginRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.Valid;
 
 /*
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -17,45 +20,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class AuthController {
-  @Autowired
-  AuthenticationManager authenticationManager;
+    @Autowired
+    AuthenticationManager authenticationManager;
 
-  @Autowired
-  PasswordEncoder encoder;
+    @Autowired
+    PasswordEncoder encoder;
 
-  @Autowired
-  JwtUtils jwtUtils;
+    @Autowired
+    JwtUtils jwtUtils;
 
-/*  @PostMapping("/signin")
-  public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, @RequestParam(required = true) String clientId) {
+    @Value("${hanss.app.authUrl}")
+    private String authUrl;
 
-    Authentication authentication = authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+    @Value("${hanss.app.jwtClientId}")
+    private String jwtClientId;
 
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = jwtUtils.generateJwtToken(authentication, clientId);
-    
-    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();    
-    List<String> roles = userDetails.getAuthorities().stream()
-        .map(item -> item.getAuthority())
-        .collect(Collectors.toList());
+    @PostMapping("/login")
+    @Operation(summary = "Login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, @RequestParam(required = true) String clientId) {
+        String URL_AUTH = "http://%1$s/api/auth/signin?clientId=%2$s";
+        String url = String.format(URL_AUTH, this.authUrl, clientId);
 
-    return ResponseEntity.ok(new JwtResponse(jwt, 
-                         userDetails.getId(), 
-                         userDetails.getUsername(), 
-                         userDetails.getEmail(), 
-                         roles));
-  }*/
+        RestTemplate restTemplate = new RestTemplate();
 
-  @GetMapping("/test")
-  @Operation(summary = "Test endpoint", security = @SecurityRequirement(name = "bearerAuth"))
-  public ResponseEntity<?> registerUser() {
-    return ResponseEntity.ok(("Passed"));
-  }
+        // Send request with GET method and default Headers.
+        String result = restTemplate.postForObject(url, loginRequest, String.class);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.badRequest().body(null);
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
+
+    @GetMapping("/test")
+    @Operation(summary = "Test endpoint", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity<?> registerUser() {
+        return ResponseEntity.ok(("Passed"));
+    }
 
 
-  //@PostMapping("/signup")
-  //@Operation(summary = "Signup endpoint", security = @SecurityRequirement(name = "bearerAuth"))
+    //@PostMapping("/signup")
+    //@Operation(summary = "Signup endpoint", security = @SecurityRequirement(name = "bearerAuth"))
  /* public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
     if (userRepository.existsByUsername(signUpRequest.getUsername())) {
       return ResponseEntity

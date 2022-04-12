7a4853613ca4abe977f1898182cab24b3ceaf7a3
hanss <hanss@list.ru>
2022-04-12 23:52:47 +0300

    Initial commit

--- src/main/java/com/hanss/gcash/model/Account.java
+++ src/main/java/com/hanss/gcash/model/Account.java
@@ -6,19 +6,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Set;
@@ -36,6 +26,8 @@ import java.util.Set;
 @NoArgsConstructor
 public class Account {
     @Id
+    @Size(max = 32)
+    @Column(length = 32)
     @Getter
     @Setter
     @EqualsAndHashCode.Include
@@ -43,28 +35,38 @@ public class Account {
 
     @NotBlank
     @Size(max = 2048)
+    @Column(length = 2048)
     @Getter
     @Setter
     private String name;
 
     @NotBlank
     @Size(max = 2048)
+    @Column(length = 2048)
     @Getter
     @Setter
     private String account_type;
 
     @NotBlank
     @Size(max = 2048)
+    @Column(length = 2048)
     @Getter
     @Setter
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @Size(max = 32)
+    @Column(length = 32)
     @Getter
     @Setter
+    private String parent_guid;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @Getter
+    @Setter
+    @JoinColumn(name = "parent_guid", insertable = false, updatable = false)
     private Account parent;
 
-    @OneToMany(fetch = FetchType.LAZY,mappedBy = "parent")
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "parent")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Setter
     private Set<Account> children;

3f642265cc818ff4a93f93a35b9886127304b3e6
hanss <hanss@list.ru>
2022-05-01 16:10:33 +0300

    Initial commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Navigate, Route, Routes, useLocation} from "react-router-dom";
 import './App.css';
 import TransactionList from "./components/TransactionList";
 import Transaction from "./components/Transaction";
@@ -7,26 +7,35 @@ import Tree from "./components/Tree";
 import Error from "./components/Error";
 import Login from "./components/auth/Login";
 import Header from "./components/layout/Header";
+import {connect} from "react-redux";
 
-function App() {
+function App(props) {
+    const {auth} = props;
     return (
         <div className="App">
             <Header></Header>
             <Routes>
-                <PrivateRoute exact path="/" element={<TransactionList/>}/>
-                <PrivateRoute exact path="tree" element={<Tree/>}/>
-                <PrivateRoute exact path="transactions" element={<TransactionList/>}/>
-                <PrivateRoute exact path="transactions/:transactionGuid" element={<Transaction/>}/>
-                <Route exact path="login" element={<Login/>}/>
+                {auth.isLoggedIn && (
+                    <>
+                        <Route exact path="/" element={<TransactionList/>}/>
+                        <Route exact path="tree" element={<Tree/>}/>
+                        <Route exact path="transactions" element={<TransactionList/>}/>
+                        <Route exact path="transactions/:transactionGuid" element={<Transaction/>}/>
+                    </>
+                )}
+                {!auth.isLoggedIn && (
+                    <Route exact path="login" element={<Login/>}/>
+                )}
                 <Route path="*" element={<Error/>}/>
             </Routes>
         </div>
     );
 }
 
-function PrivateRoute({ children }) {
-    const auth = useAuth();
-    return auth ? children : <Navigate to="/login" />;
-}
+const mapStateToProps = (state) => {
+    return {
+        auth: state.authState,
+    };
+};
 
-export default App;
+export default connect(mapStateToProps)(App);

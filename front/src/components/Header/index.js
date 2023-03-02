e405864c1a50df1772922f89ec14be61a0706f5d
pavel.grudanov <pavel.grudanov@host>
2023-03-02 23:55:10 +0300

    Change html

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React, {useContext} from "react";
+import { useNavigate } from "react-router-dom";
 import {KeycloackContext} from "../../KeycloakContext";
 import Search from "./Search";
 import "./index.css"
 
 function Header(props) {
-    const {keycloackValue} = useContext(KeycloackContext)
+    const {keycloackValue} = useContext(KeycloackContext);
+    const navigate = useNavigate();
     return (
         <React.Fragment>
             <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
@@ -19,6 +21,7 @@ function Header(props) {
                     <div className="nav-item text-nowrap">
                         <a className="nav-link px-3"
                            onClick={() => {
+                               navigate("/gcash/");
                                keycloackValue.logout();
                            }}
                         >Sign out</a>
